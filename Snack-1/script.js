6af36aeecfd366b619f5db31ef198465a70bc56d
MiriamEvardi <miriamevardi97@gmail.com>
2024-02-05 15:31:06 +0100

    Snack-1

---
+++ Snack-1/script.js
@@ -0,0 +1,24 @@
+//Crea un array vuoto e chiedi all’utente un numero da inserire nell’array.
+//Continua a chiedere i numeri all’utente e a inserirli nell’array
+//fino a quando la somma degli elementi è minore di 50
+
+let array = [];
+let sum = 0;
+
+
+while (sum < 50) {
+    let userNumber = prompt("Inserisci un numero");
+
+    let number = + userNumber;
+
+    if (isNaN(number) || (sum + number) > 50) {
+
+        alert("Inserisci un numero valido.");
+
+    } else {
+        array.push(number);
+        sum += number;
+    }
+}
+
+document.getElementById("number-sum").innerText = "La parola più corta è: " + array;
