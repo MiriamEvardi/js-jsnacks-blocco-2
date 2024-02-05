74a3748c850a334c15b4a5aa985c649607733074
MiriamEvardi <miriamevardi97@gmail.com>
2024-02-05 19:38:52 +0100

    "styled" snack-3 and snack-4

--- Snack-4/script.js
+++ Snack-4/script.js
@@ -1,23 +1,17 @@
 //Inserisci un numero, se è pari stampa il numero,
 //se è dispari stampa il numero successivo.
 
-let userNumber = Number(prompt("Inserisci un numero o più numeri fino a raggiungere o superare 50"));
-const buttonElement = document.querySelector("button");
+let userNumber = Number(prompt("Inserisci un numero"));
 
-buttonElement.addEventListener("click",
-    function () {
-        if (!isNaN(userNumber)) {
-            if (userNumber % 2 === 0) {
+userNumber = +userNumber;
 
-                document.getElementById("number").innerText = userNumber + " è pari.";
 
-            } else {
+if (userNumber % 2 === 0) {
 
-                let nextNumber = userNumber + 1;
-                document.getElementById("number").innerText = "Il numero inserito è dispari. Il numero successivo è: " + nextNumber;
-            }
-        } else {
-            document.getElementById("number").innerText = "Inserisci un numero valido.";
-        }
-    }
-)
+    document.getElementById("number").innerText = userNumber + " è pari.";
+
+} else {
+
+    let nextNumber = userNumber + 1;
+    document.getElementById("number").innerHTML = "Il numero inserito è dispari. Il numero successivo è: <br>" + nextNumber;
+}
