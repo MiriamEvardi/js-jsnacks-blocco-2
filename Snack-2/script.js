aa4cebd83a0e7a38ee3793e08e9b799246acfd55
MiriamEvardi <miriamevardi97@gmail.com>
2024-02-05 19:06:58 +0100

    snack-2 style

--- Snack-2/script.js
+++ Snack-2/script.js
@@ -1,33 +1,39 @@
 //Genera un numero casuale tra 1 e 100
 let randomNumber = Math.floor(Math.random() * 100 + 1);
+const buttonElement = document.querySelector("button");
 
-let guessNumber;
 let userAttempt = 0;
 
-do {
-    // e poi chiedi all'utente di indovinare il numero.
-    guessNumber = prompt("Indovina il numero da 1 a 100");
 
-    if (isNaN(guessNumber) || guessNumber < 1 || guessNumber > 100) {
+buttonElement.addEventListener('click',
+    function () {
+        do {
+            // e poi chiedi all'utente di indovinare il numero.
+            let guessNumber = Number(document.getElementById("userGuess").value);
+            let messageElement = document.getElementById("message");
 
-        alert("Inserisci un numero valido!");
+            if (isNaN(guessNumber) || guessNumber < 1 || guessNumber > 100) {
 
-    } else {
-        userAttempt++;
+                messageElement.innerText = "Inserisci un numero valido!";
+
+            } else {
+                userAttempt++;
+            }
+
+            if (guessNumber > randomNumber) {
+                //Il programma poi deve far comparire un messaggio "il numero è troppo alto"
+                messageElement.innerText = "Il numero è troppo alto. Riprova.";
+
+            } else if (guessNumber < randomNumber) {
+                //oppure "il numero è troppo basso"
+                messageElement.innerText = "Il numero è troppo basso. Riprova.";
+
+            } else {
+                //Quando l'utente trova il numero, fateglielo sapere!
+                messageElement.innerText = "Hai indovinato!";
+            }
+
+            //il ciclo si ripete finchè il numero dell'utente non è uguale al numero random
+        } while (userAttempt !== randomNumber);
     }
-
-    if (guessNumber > randomNumber) {
-        //Il programma poi deve far comparire un messaggio "il numero è troppo alto"
-        alert("Il numero è troppo alto, riprova");
-
-    } else if (guessNumber < randomNumber) {
-        //oppure "il numero è troppo basso"
-        alert("Il numero è troppo basso, riprova");
-
-    } else {
-        //Quando l'utente trova il numero, fateglielo sapere!
-        alert("Hai indovinato!");
-    }
-
-    //il ciclo si ripete finchè il numero dell'utente non è uguale al numero random
-} while (userAttempt !== randomNumber);
+)
--- Snack-4/script.js
+++ Snack-4/script.js
@@ -1,19 +1,23 @@
 //Inserisci un numero, se è pari stampa il numero,
 //se è dispari stampa il numero successivo.
 
-let userNumber = Number(prompt("Inserisci un numero"));
+let userNumber = Number(prompt("Inserisci un numero o più numeri fino a raggiungere o superare 50"));
+const buttonElement = document.querySelector("button");
 
+buttonElement.addEventListener("click",
+    function () {
+        if (!isNaN(userNumber)) {
+            if (userNumber % 2 === 0) {
 
-if (!isNaN(userNumber)) {
-    if (userNumber % 2 === 0) {
+                document.getElementById("number").innerText = userNumber + " è pari.";
 
-        document.getElementById("number").innerText = userNumber + " è pari.";
+            } else {
 
-    } else {
-
-        let nextNumber = userNumber + 1;
-        document.getElementById("number").innerText = "Il numero inserito è dispari. Il numero successivo è: " + nextNumber;
+                let nextNumber = userNumber + 1;
+                document.getElementById("number").innerText = "Il numero inserito è dispari. Il numero successivo è: " + nextNumber;
+            }
+        } else {
+            document.getElementById("number").innerText = "Inserisci un numero valido.";
+        }
     }
-} else {
-    document.getElementById("number").innerText = "Inserisci un numero valido.";
-}
+)
