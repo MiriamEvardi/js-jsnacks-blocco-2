fc07f453438dac08f2fc10480a131fb117303c6e
MiriamEvardi <miriamevardi97@gmail.com>
2024-02-05 17:59:39 +0100

    Snack-4

---
+++ Snack-4/script.js
@@ -0,0 +1,19 @@
+//Inserisci un numero, se è pari stampa il numero,
+//se è dispari stampa il numero successivo.
+
+let userNumber = Number(prompt("Inserisci un numero"));
+
+
+if (!isNaN(userNumber)) {
+    if (userNumber % 2 === 0) {
+
+        document.getElementById("number").innerText = userNumber + " è pari.";
+
+    } else {
+
+        let nextNumber = userNumber + 1;
+        document.getElementById("number").innerText = "Il numero inserito è dispari. Il numero successivo è: " + nextNumber;
+    }
+} else {
+    document.getElementById("number").innerText = "Inserisci un numero valido.";
+}
